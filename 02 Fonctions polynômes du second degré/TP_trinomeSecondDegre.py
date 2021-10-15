0a7a1d8e06faa4fca7c6cea96348e1f2bf05e6ad
NaturelEtChaud <66372886+NaturelEtChaud@users.noreply.github.com>
2021-10-15 08:54:10 +0200

    Update TP_trinomeSecondDegre.py

--- 02 Fonctions polynômes du second degré/TP_trinomeSecondDegre.py
+++ 02 Fonctions polynômes du second degré/TP_trinomeSecondDegre.py
@@ -7,8 +7,6 @@ from math import sqrt
 # tp1 ##########################################################################
 def discriminant(a,b,c):
     calcul = b**2 - 4*a*c
-    print("Le discriminant vaut ",calcul)
-    # on renvoie aussi le resultat
     return calcul
 
 # tp2 ##########################################################################
@@ -21,7 +19,6 @@ def nb_racines(a,b,c):
         nb=1
     else:
         nb=0
-    print("Il y a ",nb," racine(s)")
     return nb
 
 
@@ -29,26 +26,13 @@ def nb_racines(a,b,c):
 def racines(a,b,c):
     delta = discriminant(a,b,c)
     if delta>0:
-        nb=2
         racine1 = (-b-sqrt(delta))/(2*a)
         racine2 = (-b+sqrt(delta))/(2*a)
-        print("Il y a deux racines ",racine1," et ",racine2)
+        reponse = (2,racine1,racine2)
     # elif est la contraction de "else if"   
     elif (delta==0):
-        nb=1
-        racine1 = (-b)/(2*a)
-        racine2 = 0
-        print("Il y a une racine double ",racine1)
+        racine = (-b)/(2*a)
+        reponse = (1,racine)
     else:
-        nb,racine1, racine2 = 0,0,0
-        print("Il n'y a aucune racine")
-    return(nb,racine1,racine2)
-        
- 
-        
-# tp4 ##########################################################################
-def f_canonique(a,b,c):
-    alpha = -b / (2*a)
-    beta = -(b**2 - 4*a*c)/(4*a)
-    print("alpha =",alpha," et beta =",beta)
-    return (alpha,beta)
+        reponse = (0)
+    return reponse
